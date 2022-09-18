cdc55c6b77e37e4cf04f3255bf3b6bd7c41c814c
Kukuh TW <kukuhtw@gmail.com>
2022-09-18 22:59:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
+<?php
+
+/*
+  
+    $$\   $$\          $$\                 $$\             $$$$$$$$\ $$\      $$\ 
+$$ | $$  |         $$ |                $$ |            \__$$  __|$$ | $\  $$ |
+$$ |$$  /$$\   $$\ $$ |  $$\ $$\   $$\ $$$$$$$\           $$ |   $$ |$$$\ $$ |
+$$$$$  / $$ |  $$ |$$ | $$  |$$ |  $$ |$$  __$$\          $$ |   $$ $$ $$\$$ |
+$$  $$<  $$ |  $$ |$$$$$$  / $$ |  $$ |$$ |  $$ |         $$ |   $$$$  _$$$$ |
+$$ |\$$\ $$ |  $$ |$$  _$$<  $$ |  $$ |$$ |  $$ |         $$ |   $$$  / \$$$ |
+$$ | \$$\\$$$$$$  |$$ | \$$\ \$$$$$$  |$$ |  $$ |         $$ |   $$  /   \$$ |
+\__|  \__|\______/ \__|  \__| \______/ \__|  \__|         \__|   \__/     \__|
+
+ 
+ kukuhtw@gmail.com
+whatsapp : 62.8129893706
+https://www.linkedin.com/in/kukuhtw/
+https://www.instagram.com/kukuhtw/
+https://twitter.com/kukuhtw/
+https://www.facebook.com/kukuhtw
+https://www.facebook.com/profile.php?id=100083608342093
+ 
+*/
+
+
+?>
 <html lang="en">
  <?php include("head_register.php") ?>
   <body class="bg-dark">
